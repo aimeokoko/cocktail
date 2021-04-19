485286e371f2710387b1a9a8cae0ad1e2d880146
aimeokoko <75664741+aimeokoko@users.noreply.github.com>
2021-04-19 13:27:28 +0200

    Update app.R

--- app.R
+++ app.R
@@ -2,32 +2,27 @@
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# This app uses data from the website "https://www.1001cocktails.com/"
 
 
-# install.packages("dplyr")
-# install.packages("stringr")
-# install.packages("purrr")
-# install.packages("wordcloud")
-# install.packages("SnowballC")
-# install.packages("RColorBrewer")
-# install.packages("tm")
+
+# Required libraries Mon Apr 19 12:58:15 2021----------
+
+packages <- c("shiny", "dplyr", "rvest", "stringr", "purrr", "wordcloud")
+install.packages(setdiff(packages, rownames(installed.packages())))
 
 library(shiny)
+library(rvest)
 library(dplyr)
 library(stringr)
 library(purrr)
+library(wordcloud)
 
-library("tm")
-library("SnowballC")
-library("wordcloud")
-library("RColorBrewer")
+#Data Mon Apr 19 12:59:14 2021----------
+## Can be found on my github "https://github.com/aimeokoko/cocktail
 
-final_cock <- read.csv("cocktails2.csv")
-d <- read.csv("words.csv")
+final_cock <- read.csv("https://raw.githubusercontent.com/aimeokoko/cocktail/main/cocktails2.csv")
+d <- read.csv("https://raw.githubusercontent.com/aimeokoko/cocktail/main/words.csv")
 
 
 # Define UI for application that draws a histogram
@@ -57,11 +52,11 @@ ui <- fluidPage(
             h3("Ingrédients"),
             htmlOutput("ingre"),
            
-            h3("Recette"),
-            htmlOutput("rec"),
-
-            h3("Nuage de mots"),
-            plotOutput("word")
+           h3("Recette"),
+           htmlOutput("rec"),
+           
+           h3("Nuage de mots"),
+           plotOutput("word")
            
            
         )
@@ -70,6 +65,10 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(session, input, output) {
+    
+    grp <- reactive({final_cock %>% 
+            filter(Groupe == input$group) %>% 
+            select(Cocktail)})
     
     observeEvent(input$group, {
         choi <- final_cock %>% 
@@ -81,14 +80,14 @@ server <- function(session, input, output) {
 
     output$ingre <- renderUI({
         # generate bins based on input$bins from ui.R
-        ingred <- final_cock %>% filter(Cocktail == input$cock) %>%
+        ingred <- final_cock %>% filter(Cocktail == input$cock) %>% 
             select(Ingrédients) %>% reduce(paste)
             HTML(paste(ingred, collapse = "<br/>"))
     })
     output$rec <- renderUI({
-        recet <- final_cock %>% filter(Cocktail == input$cock) %>%
-            select(Recette) %>% slice(1) %>% as.character() %>%
-            str_remove_all("\t") %>% str_sub(2,-1) %>%
+        recet <- final_cock %>% filter(Cocktail == input$cock) %>% 
+            select(Recette) %>% slice(1) %>% as.character() %>% 
+            str_remove_all("\t") %>% str_sub(2,-1) %>% 
             str_replace_all("\n", "<br/> <br/>")
         HTML(recet)
     })
@@ -96,19 +95,21 @@ server <- function(session, input, output) {
     output$word <- renderPlot({
         set.seed(1234)
         wordcloud(words = d$word, freq = d$freq, min.freq = 1,
-          max.words=209, random.order=FALSE, rot.per=0.35,
+          max.words=209, random.order=FALSE, rot.per=0.35, 
           colors=brewer.pal(8, "Dark2"))
         }, width = 600, height = 400)
     
     
     output$img <- renderUI({
-        link <- final_cock %>%
-            filter(Cocktail == input$cock) %>%
+        link <- final_cock %>% 
+            filter(Cocktail == input$cock) %>% 
             select(Images) %>% slice(1) %>% as.character()
         tags$img(src = link)
     })
     
 }
+
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
