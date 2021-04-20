8895dce8c324c26251c998b78a836ca5515e79f3
aimeokoko <75664741+aimeokoko@users.noreply.github.com>
2021-04-20 18:24:18 +0200

    Update my_cocktail_app.R

--- my_cocktail_app.R
+++ my_cocktail_app.R
@@ -1,8 +1,8 @@
 packages <- "shiny"
 install.packages(setdiff(packages, rownames(installed.packages())))
-library(shiny)
+
 run <- function(){
-  runApp(temp)}
+  shiny::runApp(temp)}
 
 temp <- tempfile(fileext = ".R")
 download.file("https://raw.githubusercontent.com/aimeokoko/cocktail/main/cocktail_app.R", 
